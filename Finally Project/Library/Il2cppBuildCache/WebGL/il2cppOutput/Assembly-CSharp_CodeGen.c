c7fc7662721f94d8ed0b693d407368cac24b69d3
Lei Yang <102606797+12346546464645@users.noreply.github.com>
2022-04-20 03:15:43 +0800

    Ray integrates all projects
    Because the file is too large， upload it in batches

---
+++ Finally Project/Library/Il2cppBuildCache/WebGL/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,263 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void CoinControllerRey::Start()
+extern void CoinControllerRey_Start_m856F2FF4BEE6B5180532F2D5F645BEA0B4746BC0 (void);
+// 0x00000002 System.Void CoinControllerRey::Update()
+extern void CoinControllerRey_Update_m3ECC286DBFA3D0393C6B83874D4B3AA8F48FF6F7 (void);
+// 0x00000003 System.Void CoinControllerRey::OnTriggerEnter(UnityEngine.Collider)
+extern void CoinControllerRey_OnTriggerEnter_mD35D09ED52AAA7CD5E9865A3298E1D80B69880FB (void);
+// 0x00000004 System.Void CoinControllerRey::.ctor()
+extern void CoinControllerRey__ctor_m330758FBECB1C7D633FAF1BA72A7C30846FCC110 (void);
+// 0x00000005 System.Void GameControllerRey::Start()
+extern void GameControllerRey_Start_m49D75A5D4239A1DD83B4DF9BEB900DACB6A6530F (void);
+// 0x00000006 System.Void GameControllerRey::Update()
+extern void GameControllerRey_Update_m57E7F2845E93C409D5589965114857402AA67A79 (void);
+// 0x00000007 System.Void GameControllerRey::SetCoin()
+extern void GameControllerRey_SetCoin_mA4C24E21EF177036252C81006166BE27FFE7387D (void);
+// 0x00000008 System.Void GameControllerRey::Again()
+extern void GameControllerRey_Again_m0ADCD3AD2CCFFCF4C46A5DD6C4B2B5F4C2DCDF8B (void);
+// 0x00000009 System.Void GameControllerRey::Quit()
+extern void GameControllerRey_Quit_m736B0593B71FCE3B1171163D3A1E2105B1E8EBB9 (void);
+// 0x0000000A System.Void GameControllerRey::.ctor()
+extern void GameControllerRey__ctor_m032C033CDFA70EE65FFD168636B227736B16D8C9 (void);
+// 0x0000000B System.Void GameControllerRey::.cctor()
+extern void GameControllerRey__cctor_m92EB43AE2790A89496C742FBE68E2B574A020959 (void);
+// 0x0000000C System.Void PlayerMoveRey::Start()
+extern void PlayerMoveRey_Start_mE33CD13058436752B383C907A869D468BAE6519D (void);
+// 0x0000000D System.Void PlayerMoveRey::Update()
+extern void PlayerMoveRey_Update_m759C1E6BB5F3F3F2F10606E1042B279A474818FB (void);
+// 0x0000000E System.Void PlayerMoveRey::OnTriggerEnter(UnityEngine.Collider)
+extern void PlayerMoveRey_OnTriggerEnter_mA1A95A68E6B32A5348CFE89E243B42FAC10056A4 (void);
+// 0x0000000F System.Void PlayerMoveRey::.ctor()
+extern void PlayerMoveRey__ctor_mD5FE63BA0019C78356A405FCC40C9CD3D57C3921 (void);
+// 0x00000010 System.Void TheThirdPersonCamera::Start()
+extern void TheThirdPersonCamera_Start_m33283E0F83208BAA1CC664AD8839FF0FCFA34493 (void);
+// 0x00000011 System.Void TheThirdPersonCamera::LateUpdate()
+extern void TheThirdPersonCamera_LateUpdate_m025E04665CF7B8967371EC3D82A1AEE8E107B3AE (void);
+// 0x00000012 System.Void TheThirdPersonCamera::.ctor()
+extern void TheThirdPersonCamera__ctor_m6A2DDA9E2686FF8DE35E38508D8BA59C964677EA (void);
+// 0x00000013 System.Void ScenesMove::OnStartGame(System.String)
+extern void ScenesMove_OnStartGame_mFCF872C94569C33E9F78933E5F51686D494D9D9C (void);
+// 0x00000014 System.Void ScenesMove::.ctor()
+extern void ScenesMove__ctor_mE0F4C1F54B06066299CC0C7834A0C96ECC94E187 (void);
+// 0x00000015 System.Void GameEnding::OnTriggerEnter(UnityEngine.Collider)
+extern void GameEnding_OnTriggerEnter_m87780FB176345B677FBDCB1CF4C50A0888800471 (void);
+// 0x00000016 System.Void GameEnding::CaughtPlayer()
+extern void GameEnding_CaughtPlayer_mC2C71F5AC36218A37F16C993BD343829AE2F6132 (void);
+// 0x00000017 System.Void GameEnding::Update()
+extern void GameEnding_Update_m120E149C8CC72CE194DFB943A97709575958D142 (void);
+// 0x00000018 System.Void GameEnding::EndLevel(UnityEngine.CanvasGroup,System.Boolean,UnityEngine.AudioSource)
+extern void GameEnding_EndLevel_m07541912C8063B8BA6B15E19FF8E46EDB13B1594 (void);
+// 0x00000019 System.Void GameEnding::.ctor()
+extern void GameEnding__ctor_mDF775B6FCFF45EF72E175030518298389D957F34 (void);
+// 0x0000001A System.Void Observer::OnTriggerEnter(UnityEngine.Collider)
+extern void Observer_OnTriggerEnter_m8A32D16C3C592D1CDF69B46AF25FFA5BDBB19B10 (void);
+// 0x0000001B System.Void Observer::OnTriggerExit(UnityEngine.Collider)
+extern void Observer_OnTriggerExit_mD852F0FAB4256C02B5680E1310DF350528110069 (void);
+// 0x0000001C System.Void Observer::Update()
+extern void Observer_Update_mD03987999F861A59B23749CDEA96596323F9D649 (void);
+// 0x0000001D System.Void Observer::.ctor()
+extern void Observer__ctor_m64049426C599F37763A505365E32A802FA64264F (void);
+// 0x0000001E System.Void Pickup::Start()
+extern void Pickup_Start_m09D27020F3577A9A96B57A8873B8248828DC4EB1 (void);
+// 0x0000001F System.Void Pickup::Update()
+extern void Pickup_Update_m85A7438FA1C5E3469848BB0A9CAD4A63430F1627 (void);
+// 0x00000020 System.Void Pickup::.ctor()
+extern void Pickup__ctor_m6649ADEB2ABDE841407A83DC4682A210AFD8A93B (void);
+// 0x00000021 System.Void PlayerMovement::Start()
+extern void PlayerMovement_Start_mB585552228B1908E44D3A69496598FB485F608B6 (void);
+// 0x00000022 System.Void PlayerMovement::FixedUpdate()
+extern void PlayerMovement_FixedUpdate_m774280268A537B6ED9D9171CEAE67E9A0C3A9499 (void);
+// 0x00000023 System.Void PlayerMovement::OnAnimatorMove()
+extern void PlayerMovement_OnAnimatorMove_m7913AC2E561ABF9136444B82FC7FA278F1EBD25A (void);
+// 0x00000024 System.Void PlayerMovement::OnTriggerEnter(UnityEngine.Collider)
+extern void PlayerMovement_OnTriggerEnter_m3843EE5DA10397BDD598BD071D90B7F53873A9AD (void);
+// 0x00000025 System.Void PlayerMovement::SetCountText()
+extern void PlayerMovement_SetCountText_m5D44D208601E63B04C334B494BB0FE5D2D89B55F (void);
+// 0x00000026 System.Void PlayerMovement::EndLevel(UnityEngine.AudioSource)
+extern void PlayerMovement_EndLevel_mDADE575CFAE96AF178C957B87C5874E103CC43CC (void);
+// 0x00000027 System.Void PlayerMovement::.ctor()
+extern void PlayerMovement__ctor_mBF9F632DD9929DD6FF092A968649A4406BFE397F (void);
+// 0x00000028 System.Void WaypointPatrol::Start()
+extern void WaypointPatrol_Start_m497127FB0E2962E24BC18EADE7FA4DE6F80A6606 (void);
+// 0x00000029 System.Void WaypointPatrol::Update()
+extern void WaypointPatrol_Update_m4FCAE29E3B278293FE1984F50EDAF94169C37F8D (void);
+// 0x0000002A System.Void WaypointPatrol::.ctor()
+extern void WaypointPatrol__ctor_m52AC8FA137BDD414C942AFCD75E03D30D9BED6E2 (void);
+// 0x0000002B System.Void GameManager::Start()
+extern void GameManager_Start_m26461AEF27E44DB8FECCBC19D6C9E228B658BF8E (void);
+// 0x0000002C System.Void GameManager::Update()
+extern void GameManager_Update_mC9303BA7C3117BD861F49F8E36151CC52117E6C1 (void);
+// 0x0000002D System.Void GameManager::SetScore(System.Int32)
+extern void GameManager_SetScore_mD7FBF7DE88836ED453F80306C2903AEC02C53189 (void);
+// 0x0000002E System.Void GameManager::.ctor()
+extern void GameManager__ctor_mE8666F6D0CA9C31E16B719F79780DC4B0245B64D (void);
+// 0x0000002F System.Void GameManager::.cctor()
+extern void GameManager__cctor_m0872BB5B2E20B9183F004EB81D5129608114D285 (void);
+// 0x00000030 System.Void Player01::Start()
+extern void Player01_Start_m32149C35297A86B89A0D15CA73AA8037A5AC6290 (void);
+// 0x00000031 System.Void Player01::Update()
+extern void Player01_Update_m5780599373666F2047861FB493BB375C6C440D55 (void);
+// 0x00000032 System.Void Player01::OnTriggerEnter(UnityEngine.Collider)
+extern void Player01_OnTriggerEnter_m93D888A6DA174C4E3395C57AFDE7B14DCDCC4EAB (void);
+// 0x00000033 System.Void Player01::.ctor()
+extern void Player01__ctor_mD84B419FC54951F985E7A909F0474D29A8313ABB (void);
+// 0x00000034 System.Void StartPanel::Start()
+extern void StartPanel_Start_m949D8A96B2927CBAF2B1E3548E42ED446B4051EC (void);
+// 0x00000035 System.Void StartPanel::GameAgainButtonClickListener()
+extern void StartPanel_GameAgainButtonClickListener_mEC2338099E1F9F19BDA11AE22989829FEB097711 (void);
+// 0x00000036 System.Void StartPanel::GameQuitButtonClickListener()
+extern void StartPanel_GameQuitButtonClickListener_mF8C1C5989CBC702AF14D3329D3EEAAA28820C1D9 (void);
+// 0x00000037 System.Void StartPanel::.ctor()
+extern void StartPanel__ctor_m85B593688568B064456EEDF55E59396E59957183 (void);
+static Il2CppMethodPointer s_methodPointers[55] = 
+{
+	CoinControllerRey_Start_m856F2FF4BEE6B5180532F2D5F645BEA0B4746BC0,
+	CoinControllerRey_Update_m3ECC286DBFA3D0393C6B83874D4B3AA8F48FF6F7,
+	CoinControllerRey_OnTriggerEnter_mD35D09ED52AAA7CD5E9865A3298E1D80B69880FB,
+	CoinControllerRey__ctor_m330758FBECB1C7D633FAF1BA72A7C30846FCC110,
+	GameControllerRey_Start_m49D75A5D4239A1DD83B4DF9BEB900DACB6A6530F,
+	GameControllerRey_Update_m57E7F2845E93C409D5589965114857402AA67A79,
+	GameControllerRey_SetCoin_mA4C24E21EF177036252C81006166BE27FFE7387D,
+	GameControllerRey_Again_m0ADCD3AD2CCFFCF4C46A5DD6C4B2B5F4C2DCDF8B,
+	GameControllerRey_Quit_m736B0593B71FCE3B1171163D3A1E2105B1E8EBB9,
+	GameControllerRey__ctor_m032C033CDFA70EE65FFD168636B227736B16D8C9,
+	GameControllerRey__cctor_m92EB43AE2790A89496C742FBE68E2B574A020959,
+	PlayerMoveRey_Start_mE33CD13058436752B383C907A869D468BAE6519D,
+	PlayerMoveRey_Update_m759C1E6BB5F3F3F2F10606E1042B279A474818FB,
+	PlayerMoveRey_OnTriggerEnter_mA1A95A68E6B32A5348CFE89E243B42FAC10056A4,
+	PlayerMoveRey__ctor_mD5FE63BA0019C78356A405FCC40C9CD3D57C3921,
+	TheThirdPersonCamera_Start_m33283E0F83208BAA1CC664AD8839FF0FCFA34493,
+	TheThirdPersonCamera_LateUpdate_m025E04665CF7B8967371EC3D82A1AEE8E107B3AE,
+	TheThirdPersonCamera__ctor_m6A2DDA9E2686FF8DE35E38508D8BA59C964677EA,
+	ScenesMove_OnStartGame_mFCF872C94569C33E9F78933E5F51686D494D9D9C,
+	ScenesMove__ctor_mE0F4C1F54B06066299CC0C7834A0C96ECC94E187,
+	GameEnding_OnTriggerEnter_m87780FB176345B677FBDCB1CF4C50A0888800471,
+	GameEnding_CaughtPlayer_mC2C71F5AC36218A37F16C993BD343829AE2F6132,
+	GameEnding_Update_m120E149C8CC72CE194DFB943A97709575958D142,
+	GameEnding_EndLevel_m07541912C8063B8BA6B15E19FF8E46EDB13B1594,
+	GameEnding__ctor_mDF775B6FCFF45EF72E175030518298389D957F34,
+	Observer_OnTriggerEnter_m8A32D16C3C592D1CDF69B46AF25FFA5BDBB19B10,
+	Observer_OnTriggerExit_mD852F0FAB4256C02B5680E1310DF350528110069,
+	Observer_Update_mD03987999F861A59B23749CDEA96596323F9D649,
+	Observer__ctor_m64049426C599F37763A505365E32A802FA64264F,
+	Pickup_Start_m09D27020F3577A9A96B57A8873B8248828DC4EB1,
+	Pickup_Update_m85A7438FA1C5E3469848BB0A9CAD4A63430F1627,
+	Pickup__ctor_m6649ADEB2ABDE841407A83DC4682A210AFD8A93B,
+	PlayerMovement_Start_mB585552228B1908E44D3A69496598FB485F608B6,
+	PlayerMovement_FixedUpdate_m774280268A537B6ED9D9171CEAE67E9A0C3A9499,
+	PlayerMovement_OnAnimatorMove_m7913AC2E561ABF9136444B82FC7FA278F1EBD25A,
+	PlayerMovement_OnTriggerEnter_m3843EE5DA10397BDD598BD071D90B7F53873A9AD,
+	PlayerMovement_SetCountText_m5D44D208601E63B04C334B494BB0FE5D2D89B55F,
+	PlayerMovement_EndLevel_mDADE575CFAE96AF178C957B87C5874E103CC43CC,
+	PlayerMovement__ctor_mBF9F632DD9929DD6FF092A968649A4406BFE397F,
+	WaypointPatrol_Start_m497127FB0E2962E24BC18EADE7FA4DE6F80A6606,
+	WaypointPatrol_Update_m4FCAE29E3B278293FE1984F50EDAF94169C37F8D,
+	WaypointPatrol__ctor_m52AC8FA137BDD414C942AFCD75E03D30D9BED6E2,
+	GameManager_Start_m26461AEF27E44DB8FECCBC19D6C9E228B658BF8E,
+	GameManager_Update_mC9303BA7C3117BD861F49F8E36151CC52117E6C1,
+	GameManager_SetScore_mD7FBF7DE88836ED453F80306C2903AEC02C53189,
+	GameManager__ctor_mE8666F6D0CA9C31E16B719F79780DC4B0245B64D,
+	GameManager__cctor_m0872BB5B2E20B9183F004EB81D5129608114D285,
+	Player01_Start_m32149C35297A86B89A0D15CA73AA8037A5AC6290,
+	Player01_Update_m5780599373666F2047861FB493BB375C6C440D55,
+	Player01_OnTriggerEnter_m93D888A6DA174C4E3395C57AFDE7B14DCDCC4EAB,
+	Player01__ctor_mD84B419FC54951F985E7A909F0474D29A8313ABB,
+	StartPanel_Start_m949D8A96B2927CBAF2B1E3548E42ED446B4051EC,
+	StartPanel_GameAgainButtonClickListener_mEC2338099E1F9F19BDA11AE22989829FEB097711,
+	StartPanel_GameQuitButtonClickListener_mF8C1C5989CBC702AF14D3329D3EEAAA28820C1D9,
+	StartPanel__ctor_m85B593688568B064456EEDF55E59396E59957183,
+};
+static const int32_t s_InvokerIndices[55] = 
+{
+	1944,
+	1944,
+	1604,
+	1944,
+	1944,
+	1944,
+	1944,
+	1944,
+	1944,
+	1944,
+	3038,
+	1944,
+	1944,
+	1604,
+	1944,
+	1944,
+	1944,
+	1944,
+	1604,
+	1944,
+	1604,
+	1944,
+	1944,
+	614,
+	1944,
+	1604,
+	1604,
+	1944,
+	1944,
+	1944,
+	1944,
+	1944,
+	1944,
+	1944,
+	1944,
+	1604,
+	1944,
+	1604,
+	1944,
+	1944,
+	1944,
+	1944,
+	1944,
+	1944,
+	1591,
+	1944,
+	3038,
+	1944,
+	1944,
+	1604,
+	1944,
+	1944,
+	1944,
+	1944,
+	1944,
+};
+extern const CustomAttributesCacheGenerator g_AssemblyU2DCSharp_AttributeGenerators[];
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	55,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	g_AssemblyU2DCSharp_AttributeGenerators,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
---
+++ Finally Project/Library/Il2cppBuildCache/WebGL/il2cppOutput/lumpedcpp/Lump_libil2cpp_codegen.cpp
@@ -0,0 +1,4 @@
+#include "il2cpp-config.h"
+#include "G:\Unity\123\2020.3.30f1c1\Editor\Data\il2cpp\libil2cpp\codegen\il2cpp-codegen-common.cpp"
+#include "G:\Unity\123\2020.3.30f1c1\Editor\Data\il2cpp\libil2cpp\codegen\il2cpp-codegen-il2cpp.cpp"
+#include "G:\Unity\123\2020.3.30f1c1\Editor\Data\il2cpp\libil2cpp\codegen\il2cpp-codegen-tiny.cpp"
